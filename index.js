19df8809d26f2378d55807431c7915d560bf286f
hasan-naim <hasannaim6246@gmail.com>
2023-01-02 16:15:43 +0600

    vercel bug fixing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 /// middleware for getting data from client side
 app.use(express.json());
